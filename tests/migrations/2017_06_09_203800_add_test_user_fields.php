a54d02cfc762850998698bcfbf5cce4cd065f415
Michael Dyrynda <michael@dyrynda.com.au>
2017-06-09 22:05:50 +0930

    add test user field migration

---
+++ tests/migrations/2017_06_09_203800_add_test_user_fields.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTestUserFields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('admin')->default(false);
+            $table->string('force_filled')->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
