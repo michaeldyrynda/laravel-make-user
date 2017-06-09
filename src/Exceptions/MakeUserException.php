721bd35bddbacb4babb242125eede20f50d657bc
Michael Dyrynda <michael@dyrynda.com.au>
2017-06-09 15:54:28 +0930

    add exception class

---
+++ src/Exceptions/MakeUserException.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Dyrynda\Artisan\Exceptions;
+
+use InvalidArgumentException;
+
+class MakeUserException extends InvalidArgumentException
+{
+    /**
+     * The supplied email is invalid.
+
+     * @param  string  $email
+     * @return \Dyrynda\Artisan\Exceptions\MakeUserException
+     */
+    public static function invalidEmail($email)
+    {
+        return new static("The email address [{$email}] is invalid");
+    }
+
+    /**
+     * The supplied email already exists.
+     *
+     * @param  string $email
+     * @return \Dyrynda\Artisan\Exceptions\MakeUserException
+     */
+    public static function emailExists($email)
+    {
+        return new static("A user with the email address {$email} already exists");
+    }
+}
