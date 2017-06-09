2e1fc1b86bc4ea0d7fc5efe54856ea58fdc48cac
Michael Dyrynda <michael@dyrynda.com.au>
2017-06-09 15:54:00 +0930

    simplify service provider

--- src/MakeUserServiceProvider.php
+++ src/MakeUserServiceProvider.php
@@ -3,24 +3,14 @@
 namespace Dyrynda\Artisan;
 
 use Illuminate\Support\ServiceProvider;
+use Dyrynda\Artisan\Console\Commands\MakeUser;
 
 class MakeUserServiceProvider extends ServiceProvider
 {
-    public function boot()
-    {
-        $this->publishes([
-            __DIR__.'/../config/laravel-make-user.php' => config_path('laravel-make-user.php'),
-        ]);
-    }
-
     public function register()
     {
-        $this->mergeConfigFrom(
-            __DIR__'../config/laravel-make-user.php', 'laravel-make-user'
-        );
-
         $this->app->singleton('dyrynda.artisan.make.user', function ($app) {
-            return $app->make(Dyrynda\Artisan\Console\Commands\MakeUser::class);
+            return $app->make(MakeUser::class);
         });
 
         $this->commands('dyrynda.artisan.make.user');
