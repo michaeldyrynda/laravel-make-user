5bdbcb2957b0f14f82b6c066db57477aa1ade6e3
Craig Harman <craig@harmonicnewmedia.com>
2019-06-04 14:50:57 +0800

    Modified tests to add password encryption question

--- tests/MakeUserTest.php
+++ tests/MakeUserTest.php
@@ -2,26 +2,25 @@
 
 namespace Tests;
 
-class MakeUserTest extends TestCase
-{
+class MakeUserTest extends TestCase {
     /** @test */
-    public function it_creates_a_new_user()
-    {
+    public function it_creates_a_new_user() {
         $this->artisan('make:user')
             ->expectsQuestion("What is the new user's email address?", 'user@example.com')
             ->expectsQuestion("What is the new user's name?", 'Test User')
             ->expectsQuestion("What is the new user's password? (blank generates a random one)", '')
+            ->expectsQuestion('Should the password be encrypted?', 'yes')
             ->expectsQuestion('Do you want to send a password reset email?', 'no')
             ->expectsQuestion('Do you have any custom user fields to add? Field=Value (blank continues)', '');
     }
 
     /** @test */
-    public function it_creates_a_new_user_with_additional_fields()
-    {
+    public function it_creates_a_new_user_with_additional_fields() {
         $this->artisan('make:user')
             ->expectsQuestion("What is the new user's email address?", 'user@example.com')
             ->expectsQuestion("What is the new user's name?", 'Test User')
             ->expectsQuestion("What is the new user's password? (blank generates a random one)", '')
+            ->expectsQuestion('Should the password be encrypted?', 'yes')
             ->expectsQuestion('Do you want to send a password reset email?', 'no')
             ->expectsQuestion('Do you have any custom user fields to add? Field=Value (blank continues)', 'field=value')
             ->expectsQuestion('Do you have any custom user fields to add? Field=Value (blank continues)', '');
