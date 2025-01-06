adcc7563360e9268e77e374a89248f5d19d02f6a
Laravel Shift <shift@laravelshift.com>
2025-01-06 18:17:17 -0500

    Laravel 11.x Compatibility (#20)
    Co-authored-by: Michael Dyrynda <michael@dyrynda.com.au>

--- tests/MakeUserTest.php
+++ tests/MakeUserTest.php
@@ -1,31 +1,22 @@
 <?php
 
-namespace Tests;
+it('creates a new user', function () {
+    $this->artisan('make:user')
+        ->expectsQuestion("What is the new user's email address?", 'user@example.com')
+        ->expectsQuestion("What is the new user's name?", 'Test User')
+        ->expectsQuestion("What is the new user's password? (blank generates a random one)", '')
+        ->expectsQuestion('Should the password be encrypted?', 'yes')
+        ->expectsQuestion('Do you want to send a password reset email?', 'no')
+        ->expectsQuestion('Do you have any custom user fields to add? Field=Value (blank continues)', '');
+});
 
-class MakeUserTest extends TestCase
-{
-    /** @test */
-    public function it_creates_a_new_user()
-    {
-        $this->artisan('make:user')
-            ->expectsQuestion("What is the new user's email address?", 'user@example.com')
-            ->expectsQuestion("What is the new user's name?", 'Test User')
-            ->expectsQuestion("What is the new user's password? (blank generates a random one)", '')
-            ->expectsQuestion('Should the password be encrypted?', 'yes')
-            ->expectsQuestion('Do you want to send a password reset email?', 'no')
-            ->expectsQuestion('Do you have any custom user fields to add? Field=Value (blank continues)', '');
-    }
-
-    /** @test */
-    public function it_creates_a_new_user_with_additional_fields()
-    {
-        $this->artisan('make:user')
-            ->expectsQuestion("What is the new user's email address?", 'user@example.com')
-            ->expectsQuestion("What is the new user's name?", 'Test User')
-            ->expectsQuestion("What is the new user's password? (blank generates a random one)", '')
-            ->expectsQuestion('Should the password be encrypted?', 'yes')
-            ->expectsQuestion('Do you want to send a password reset email?', 'no')
-            ->expectsQuestion('Do you have any custom user fields to add? Field=Value (blank continues)', 'field=value')
-            ->expectsQuestion('Do you have any custom user fields to add? Field=Value (blank continues)', '');
-    }
-}
+it('creates a new user with additional fields', function () {
+    $this->artisan('make:user')
+        ->expectsQuestion("What is the new user's email address?", 'user@example.com')
+        ->expectsQuestion("What is the new user's name?", 'Test User')
+        ->expectsQuestion("What is the new user's password? (blank generates a random one)", '')
+        ->expectsQuestion('Should the password be encrypted?', 'yes')
+        ->expectsQuestion('Do you want to send a password reset email?', 'no')
+        ->expectsQuestion('Do you have any custom user fields to add? Field=Value (blank continues)', 'field=value')
+        ->expectsQuestion('Do you have any custom user fields to add? Field=Value (blank continues)', '');
+});
